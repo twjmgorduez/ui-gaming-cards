51ee830459f285537bbda8f071505f8818754663
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-15 13:40:51 -0500

    Defining project struture

---
+++ src/main/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import NewMessageForm from './NewMessageForm';
+import MessageList from './MessageList';
+
+const App = () => {
+
+  const [messages, setMessages] = useState([]);
+  const handleSend = newMessage => {
+    setMessages([newMessage, ...messages]);
+  };
+
+  return (
+    <div>
+      <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </div>
+  );
+};
+
+export default App;
